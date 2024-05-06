f2c65ad7da92fce589d7856e0d53a086b186da5e
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-05-06 17:49:01 +0500

    Update manual_deploy.sh

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -3,9 +3,15 @@ echo "Hello, $USER!"
 echo "Today is $(date)"
 echo "begin starting babah system"
 
+#to create backup directory
+cd /home/programfid/app/
+if [ ! -d "backup" ]; then
+  mkdir -pv backup
+fi
+
 #let's create a backup copy
-source_dir="/etc/systemd/system/"
-backup_dir="/home/programfid/app/archive"
+source_dir="/home/programfid/app/"
+backup_dir="/home/programfid/app/backup/"
 timestamp =$(date +%Y% m%d%H%M%S)
 backup_file="backup_$timestamp.tar.gz"
 tar -czvf"$backup_dir/$backup_file""$source_dir"
