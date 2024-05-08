ff627915002d832c051a35c1a8c778f0ac8b9d87
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-08 09:40:00 +0500

    start_servis

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+set -e
+log=log-$(date +"%Y-%m-%d")
+#cd /home/programfid/app; nohup ./run.sh >> ./logs/$log &2>1 &
+cd /home/programfid/app && source ./venv/bin/activate && flask run
+start.sh (END)
