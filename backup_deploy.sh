9f6bf19c7d7c0ca7d32bd321ab7c9fa6732db624
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-12 10:40:52 +0500

    lesson docker

--- backup_deploy.sh
+++ backup_deploy.sh
@@ -18,8 +18,8 @@ timestamp=$(date +"%Y-%m-%d-%h-%m-%s")
 dfile=$(find . -maxdepth 1 -type f -name "*.sh" -o -name "*.py")
 backup_file="$timestamp_$dfile.tar.gz"
 echo $dfile
-tar -czvf "$backup_dir/$backup_file $dfile"
-#find $backup_dir"*.tar.gz" -mtime +10 -exec rm {} \;
+tar -czvf "$backup_dir/$backup_file" "$dfile"
+#find $backup_dir -name "*.tar.gz" -mtime +10 -exec rm {} \;
 
 backup_deploy.sh (END)
 
