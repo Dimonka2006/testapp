638bdee563607e95c72717ac26dbd22e83fd4d35
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-14 18:03:18 +0500

    cloun

--- backup_deploy.sh
+++ backup_deploy.sh
@@ -11,16 +11,15 @@ if [ ! -d "backup" ]; then
 fi
 
 #let's create a backup copy
-
 source_dir="/home/programfid/app/"
 backup_dir="/home/programfid/app/backup/"
 fdate=$(date +"%Y-%m-%d-%h-%m")
 dfile=$(find . -maxdepth 1 -type f -name "*.sh" -o -name "*.py")
 bfile="$fdate_$dfile.tar.gz"
 echo $dfile
-#tar -czvf "$backup_dir/$bfile" "$dfile"
-#find $backup_dir -name "*.tar.gz" -mtime +10 -exec rm {} \;
-tar -czvf "/home/programfid/app/backup/"/"date +"%Y-%m-%d-%h-%m"_find -type f -name "*.sh" -o -name "*.py".tar.gz" "find -type f -name "*.sh" -o -name "*.py"
+tar -czvf "$backup_dir/$bfile" "$dfile"
+find $backup_dir -name "*.tar.gz" -mtime +10 -exec rm {} \;
+#tar -czvf "/home/programfid/app/backup/"/"date +"%Y-%m-%d-%h-%m"_find -type f -name "*.sh" -o -name "*.py".tar.gz" "find -type f -name "*.sh" -o -name "*.py"
 
 backup_deploy.sh (END)
 
