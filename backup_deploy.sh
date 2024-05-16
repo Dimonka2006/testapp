99ab120f72b837458f9ba976453a1f90fb2b8969
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-16 18:04:04 +0500

    raz

--- backup_deploy.sh
+++ backup_deploy.sh
@@ -8,6 +8,7 @@ echo "begin starting babah system"
 
 if [ ! -d "backup" ]; then
   mkdir -pv backup
+echo "create backup directory"
 fi
 
 #let's create a backup copy
@@ -15,13 +16,10 @@ fi
 source_dir="/home/programfid/app/"
 backup_dir="/home/programfid/app/backup/"
 timestamp=$(date +"%Y-%m-%d-%h-%m-%s")
-dfile=$(find . -maxdepth 1 -type f -name "*.sh" -o -name "*.py")
+dfile=$(find . -maxdepth 1 -type f -name "*.sh" -o -name "*.py" | sed 's|^./||')
 # | sed 's/ /\t/g' {} | cut -f 2
 #find . -name "*.sh" -o -name "*.py" -exec ls -f '{}' \+
 backup_file="$timestamp_$dfile.tar.gz"
 echo $dfile
 tar -czvf "$backup_dir/$backup_file" "$dfile"
 #find $backup_dir -name "*.tar.gz" -mtime +10 -exec rm {} \;
-
-backup_deploy.sh (END)
-
