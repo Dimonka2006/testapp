d55be381153563836fcd9d8975254dc2c75a6109
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-19 12:50:20 +0500

    work on bugs

--- arch.sh
+++ arch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd /home/programfid/app/
 if [ ! -d "archive" ]; then
   mkdir -pv archive
 fi
 pfile=$(find . -daystart -mtime +1 -name "*.csv" | sed 's|^./||')
-mv $pfile archive 
-cd archive
+mv $pfile /archive 
+cd /home/programfid/app/archive
 gzip *.csv
--- backup_deploy.sh
+++ backup_deploy.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-set -e
+set -x
 echo "Hello, $USER!"
 echo "Today is $(date)"
 echo "begin starting babah system"
 
 #to create backup directory
-
+cd /home/programfid/app/
 if [ ! -d "backup" ]; then
-  mkdir -pv backup
+mkdir -pv backup
 echo "create backup directory"
 fi
 
 #let's create a backup copy
-
 source_dir="/home/programfid/app/"
 backup_dir="/home/programfid/app/backup/"
 timestamp=$(date +"%Y-%m-%d-%h-%m-%s")
-dfile=$(find . -maxdepth 1 -type f -name "*.sh" -o -name "*.py" | sed 's|^./||')
-# | sed 's/ /\t/g' {} | cut -f 2
-#find . -name "*.sh" -o -name "*.py" -exec ls -f '{}' \+
-backup_file="$timestamp_$dfile.tar.gz"
+dfile=$(find . -maxdepth 1 -type f -name "*.sh" -o -name "*.py" -printf " %f ")
+#| sed 's|^./||') 
+# 2 way
+backup_file="$timestamp.tar.gz"
 echo $dfile
 tar -czvf "$backup_dir/$backup_file" "$dfile"
 #find $backup_dir -name "*.tar.gz" -mtime +10 -exec rm {} \;
