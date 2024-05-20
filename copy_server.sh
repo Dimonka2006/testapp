11e59a20141eacba82ad63f49bc7a50081e4d96a
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-20 17:59:05 +0500

    ver. 2

--- copy_server.sh
+++ copy_server.sh
@@ -2,5 +2,7 @@
 set -x
 #выложить на сервер
 scp -P 22 backup_deploy.sh dus@srv503956.hstgr.cloud:/home/programfid/app
-ssh dus@srv503956.hstgr.cloud 
-cd /home/programfid/app ./backup_deploy.sh
+ssh dus@srv503956.hstgr.cloud 'bash -s' < cd /home/programfid/app ./backup_deploy.sh
+
+
+exit 0
