0c89b6ac2f02d8da6ab9702b9bb5984b6a171f18
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-23 13:42:13 +0500

    name

--- copy_server_all.sh
+++ copy_server_all.sh
@@ -1,16 +1,19 @@
 ##!/bin/bash
 set -e
+echo "Hi, what's your name?"
+read NAME
+echo "my name is $NAME"
 #выложить на сервер backup скрипт
-scp -P 22 backup_deploy.sh $USER@srv503956.hstgr.cloud:/home/programfid/app
+scp -P 22 backup_deploy.sh $NAME@srv503956.hstgr.cloud:/home/programfid/app
 #запуск выложеного скрипта на сервере 
-ssh $USER@srv503956.hstgr.cloud 'bash -s' < "cd /home/programfid/app && ./backup_deploy.sh"
+ssh $NAME@srv503956.hstgr.cloud 'bash -s' < "cd /home/programfid/app && ./backup_deploy.sh"
 #выложить программы и базы на сервер 
-#scp -r directory user@remote_host[:dest_dir] если копировать директорию
-# rsync -nva dir user@host:dest_dir или через службу rsunc для передачи всей иерархии каталогов, 
+#scp -r directory NAME@remote_host[:dest_dir] если копировать директорию
+# rsync -nva dir NAME@host:dest_dir или через службу rsunc для передачи всей иерархии каталогов, 
 # включая символические ссылки, права доступа, режимы и устройства, используйте параметр -a
-scp -P 22 test_prog.sh $USER@srv503956.hstgr.cloud:/home/programfid/app
+scp -P 22 test_prog.sh $NAME@srv503956.hstgr.cloud:/home/programfid/app
 #переименование всех файлов на programfid
-ssh $USER@srv503956.hstgr.cloud 'bash -s' < "cd /home/programfid/app && sudo chown -R programfid:programfid ./"
+ssh $NAME@srv503956.hstgr.cloud 'bash -s' < "cd /home/programfid/app && sudo chown -R programfid:programfid ./"
 #Остановка сервиса
 sudo systemctl stop app.service
 #Обновить конфигурацию systemd:
