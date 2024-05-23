766cdb09a399d4229e50dc7b5d595ebe06185326
Dimonka2006 <Dimonka2006@mail.ru>
2024-05-23 14:04:58 +0500

    super

--- copy_server_all.sh
+++ copy_server_all.sh
@@ -6,14 +6,14 @@ echo "my name is $NAME"
 #выложить на сервер backup скрипт
 scp -P 22 backup_deploy.sh $NAME@srv503956.hstgr.cloud:/home/programfid/app
 #запуск выложеного скрипта на сервере 
-ssh $NAME@srv503956.hstgr.cloud 'bash -s' < cd /home/programfid/app && ./backup_deploy.sh
+ssh $NAME@srv503956.hstgr.cloud 'cd /home/programfid/app && ./backup_deploy.sh'
 #выложить программы и базы на сервер 
 #scp -r directory NAME@remote_host[:dest_dir] если копировать директорию
 # rsync -nva dir NAME@host:dest_dir или через службу rsunc для передачи всей иерархии каталогов, 
 # включая символические ссылки, права доступа, режимы и устройства, используйте параметр -a
 scp -P 22 test_prog.sh $NAME@srv503956.hstgr.cloud:/home/programfid/app
 #переименование всех файлов на programfid
-ssh $NAME@srv503956.hstgr.cloud 'bash -s' < cd /home/programfid/app && sudo chown -R programfid:programfid ./
+ssh $NAME@srv503956.hstgr.cloud 'cd /home/programfid/app && sudo chown -R programfid:programfid ./'
 #Остановка сервиса
 sudo systemctl stop app.service
 #Обновить конфигурацию systemd:
