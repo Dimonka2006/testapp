7fd9a8649e6758a4ef574b13d3f328412a2ace52
Dimonka2006 <Dimonka2006@mail.ru>
2024-06-03 10:59:17 +0500

    Python The first steps

--- Python_lesson/lesson_1.py
+++ Python_lesson/lesson_1.py
@@ -1,13 +1,17 @@
-a = "Hello World"
-b = 25
-c = 13
+#a = "Hello World"
+#b = 25
+#c = 13
 
-print(a)
-print(b-c)
+#print(a)
+#print(b-c)
 
-f_name = 'Dimon'
-l_name = 'Solovyev'
+#f_name = 'Dimon'
+#l_name = 'Solovyev'
 
-print(f_name + " " + l_name)
+#print(f_name + " " + l_name)
 
 #commmit
+
+
+name = input("What’s your name? ")
+print("Hey, " + name + "!")
