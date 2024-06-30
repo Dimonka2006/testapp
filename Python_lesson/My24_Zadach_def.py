88145d0ffb304513babfd70e35103ddec5d86c3b
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-06-30 11:58:31 +0500

    Task result

--- Python_lesson/My24_Zadach_def.py
+++ Python_lesson/My24_Zadach_def.py
@@ -51,3 +51,31 @@ keys_to_extract = ('president', 'collection', 'game', 'voice', 'spaceship') # К
 new_dictionary = task2('C:\\task9_dict.json', keys_to_extract)
 
 print(new_dictionary)
+
+print('_____________________________________\n')
+
+with open('C:\\task9_dict.json', 'r') as f:
+    data = json.load(f)
+
+def task4(json_data, keys_to_extract):
+
+    new_dict2 = {} # Создаем новый словарь для хранения результатов
+    for key in keys_to_extract:
+        if key in data:
+        # Преобразуем список строк в список объектов, чтобы работать с ними
+            lines = [line.strip() for line in data[key].split('\n')]
+                
+            # Выбираем первые две и последние две строки
+            first_two = lines[:2]
+            last_two = lines[-2:]
+                
+            # Добавляем результаты в новый словарь
+            new_dict2[key] = '\n'.join(first_two + last_two)
+    return new_dict2
+
+json_data = 'C:\\task9_dict.json'
+keys_to_extract = ('president', 'collection', 'game', 'voice', 'spaceship')
+new_dict2 = task4(json_data, keys_to_extract)
+print(new_dict2)
+
+print('_____________________________________\n')
