b86f70ec5bb68b6a67dff109d26238e5d5975181
Dimonka2006 <Dimonka2006@mail.ru>
2024-07-01 12:15:03 +0500

    renaming

--- Python_lesson/My25_Zadach_sp.py
+++ Python_lesson/My25_Zadach_sp.py
@@ -5,13 +5,7 @@ fruits = ["apple", "orange", "pineapple", "mango", "cherry"]
 for key in fruits:
     print(key)
 
-
-# Создайте список содержащий названия фруктов, 
-# напишите программу которая ваведет каждое название в новой строке
-fruits = ["apple", "orange", "pineapple", "mango", "cherry"]
-
 # Александр Гладков, [30.06.2024 12:28]
-fruits = ["apple", "orange", "pineapple", "mango", "cherry"]
 
 s = "\n".join(fruits)
 
@@ -29,8 +23,8 @@ def filter(fruits):
     
 print(filter(fruits))
 
-print(_____________________\n)
+print('_____________________\n')
 
-
+# Александр Гладков
 a = [ f for f in fruits if "a" in f]
 
