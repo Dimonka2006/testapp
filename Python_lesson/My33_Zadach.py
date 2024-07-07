8a6b8a84ca91c7807941f9c8f64c98b039ca398e
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-07-07 23:25:21 +0500

    решаем

--- Python_lesson/My33_Zadach.py
+++ Python_lesson/My33_Zadach.py
@@ -8,46 +8,47 @@
 
 import csv
 
+lib = []
 with open('books.csv', 'r', newline='', encoding='utf-8') as file:
     # Создаем объект writer для записи данных в файл
-    reader = csv.DictReader(file, fieldnames=['title', 'author', 'genre', 'height', 'publisher'])
-    for row in reader:
-    # читаем заголовки столбцов
-        reader.fieldnames
-        print(row['title'], row['author'], row['genre'], row['height'], row['publisher'])
-                  
+    reader = csv.DictReader(file, fieldnames=['Title', 'Author', 'Genre', 'Height', 'Publisher'])
     
-
-
-#Title,Author,Genre,Height,Publisher
-def new_book(title, author, genre, height, publisher):
-    book = {"title":title, "author":author, "genre":genre, "height":height, "publisher":publisher}
-    file.append(book)
-
-new_book("Убийство в поезде", "Агата Кристи", "Детектив", 300, "Аркаим")
-
+    for row in reader:
+        lib.append(row)
+    # читаем заголовки столбцов
+        #print(row)         
 #print(lib)
 
-def find_book(title, author, genre, height, publisher):
+
+# #Title,Author,Genre,Height,Publisher
+def new_book(title, author, genre, height, publisher):
+     book = {"Title":title, "Author":author, "Genre":genre, "Height":height, "Publisher":publisher}
+     lib.append(book)
+
+new_book("Убийство в поезде", "Агата Кристи", "Детектив", 300, "Аркаим")
+print(lib[-2:])
+# #print(lib)
+
+def find_book(title = None, author = None, genre = None, height = None, publisher = None):
     resault_sp = []
-    for book_dict in file:
-        #print(book_dict)
-        if title == book_dict["title"] or author == book_dict["author"] or genre == book_dict["genre"], or genre == book_dict["height"], or genre == book_dict["publisher"]:
+    for book_dict in lib:
+#         #print(book_dict)
+        if title == book_dict["Title"] or author == book_dict["Author"] or genre == book_dict["Genre"] or height == book_dict["Height"] or publisher == book_dict["Publisher"]:
 
             resault_sp.append(book_dict)
 
 
     return resault_sp
 
-books = find_book("Убийство в поезде", "Агата Кристи", "Детектив", 300, "Аркаим")
+books = find_book(genre = "history") # Поиск по именованному аргументу
 print(books)
 
-# with open('books.csv', 'w', newline='', encoding='utf-8') as file:
-#     # Создаем объект writer для записи данных в файл
-#     writer = csv.DictWriter(file, fieldnames=['title', 'author', 'genre', 'height', 'publisher'])
+# # with open('books1.csv', 'w', newline='', encoding='utf-8') as file:
+# #     # Создаем объект writer для записи данных в файл
+# #     writer = csv.DictWriter(file, fieldnames=['title', 'author', 'genre', 'height', 'publisher'])
     
-#     # Записываем заголовки столбцов
-#     writer.writeheader()
+# #     # Записываем заголовки столбцов
+# #     writer.writeheader()
     
-#     # Записываем данные в файл
-#     writer.writerows(file)
+# #     # Записываем данные в файл
+# #     writer.writerows(file)
