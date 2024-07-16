283682bb53337593b8610ce59927ff8e0031dc26
Dimonka2006 <Dimonka2006@mail.ru>
2024-07-16 14:13:46 +0500

    menu, search

--- Python_lesson/My36_Zadach_menu.py
+++ Python_lesson/My36_Zadach_menu.py
@@ -4,37 +4,6 @@ import sqlite3
 def spisok_command():
     pass
 
-
-
-def search():
-    # поиск в базе данных sq
-    conn = sqlite3.connect('books.db')
-    cur = conn.cursor()
-    search_author = input('whrite author ' )
-
-    query = "SELECT  *  FROM books WHERE author = search_author;"
-    cur.execute(query)
-    results = cur.fetchall()
-
-    for result in results:
-        print(result)
-
-    conn.close()
-
-    print('serching')
-
-def plus_db():
-    print('plussing')   
-
-def dell_book():
-    print('delling') 
-
-def append_book():
-    print('appending')     
-
-def kill_duble_book():
-    print('killing')      
-
 def menu():
     command = [('Search', search), ('Plus_db', plus_db), ('Dell_book', dell_book), ('Append_book', append_book), ('Kill_duble_book', kill_duble_book)]
     
@@ -55,3 +24,38 @@ def menu():
         if continue_program == 'n':
             break
 menu()
+
+def search():
+    # поиск в базе данных sq
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    # Запрашиваем у пользователя имя автора
+    author_name = input("Введите имя автора: ")
+
+    query = 'SELECT  *  FROM books WHERE Author=?'
+    cur.execute(query, (author_name,))
+    # Получаем результаты запроса
+    results = cur.fetchall()
+ 
+    for result in results:
+        print(result)
+    # Закрываем соединение с базой данных
+    cur.close()
+    conn.close()
+
+# Вызываем функцию
+search()  
+print('serching')
+
+def plus_db():
+    print('plussing')   
+
+def dell_book():
+    print('delling') 
+
+def append_book():
+    print('appending')     
+
+def kill_duble_book():
+    print('killing')      
+
