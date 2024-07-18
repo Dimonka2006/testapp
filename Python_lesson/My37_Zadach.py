1e588737abfb7a04ffb63c609156dbfa8b499477
Dimonka2006 <Dimonka2006@mail.ru>
2024-07-18 18:00:58 +0500

    demo

--- Python_lesson/My37_Zadach.py
+++ Python_lesson/My37_Zadach.py
@@ -1,2 +1,30 @@
-#1
-#
+import sqlite3
+
+def kill_duble_book():
+    print('killing')      
+
+     # Открываем соединение с базой данных
+    conn = sqlite3.connect('books.db')
+    cursor = conn.cursor()
+
+    # Читаем все записи из таблицы в список
+    cursor.execute("SELECT  *  FROM books")
+    records = cursor.fetchall()
+    
+        # Проверяем каждую запись на наличие дубликатов
+    unique_records = []
+    for record in records:
+        title, author, genre, height, publisher = record
+        if (title, author, genre, height, publisher) not in unique_records:
+            unique_records.append((title, author, genre, height, publisher))
+
+    # Удаляем дубликаты из списка
+    unique_records = list(set(unique_records))
+    print(list(set(unique_records)))
+
+#    # Записываем уникальные записи обратно в базу данных
+    cursor.executemany("INSERT OR REPLACE INTO books VALUES (?, ?, ?, ?, ?)", unique_records)
+    conn.commit()
+
+    # Закрываем соединение с базой данных
+    conn.close()
