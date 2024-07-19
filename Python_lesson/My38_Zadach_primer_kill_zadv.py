ec0510935b55371b035427383b3a28536171c349
Dimonka2006 <Dimonka2006@mail.ru>
2024-07-19 12:27:43 +0500

    norme_prog

---
+++ Python_lesson/My38_Zadach_primer_kill_zadv.py
@@ -0,0 +1,20 @@
+# Удаление повторяющихся строк и сохранение в отдельный файл
+def kill_duble_strok():
+
+    target = input("Введите ссылку на файл\n\n")
+    
+    f = open(target, 'r')
+    l = [line.strip() for line in f]
+    unique = []
+    [unique.append(item) for item in l if item not in unique]
+    if unique[0]:
+        print('\nДубликаты удалены')
+    f.close()
+
+    filename2 = target + 'unic_Sp_book.txt'
+    fw = open(filename2, 'w')
+    for index in unique:
+        fw.write(index + '\n')
+    fw.close()        
+    
+kill_duble_strok()
