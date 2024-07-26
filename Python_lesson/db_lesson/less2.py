e46c052c49b0c7cfedcbe58ae5114faf14dc130c
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-07-26 14:48:50 +0500

    ucheba DB

---
+++ Python_lesson/db_lesson/less2.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+# connect - сздает соединение с БД, создает ее и соединяет нас с ней
+with sqlite3.connect('test2.db') as con:
+    cur = con.cursor() # cursor() - создает обект через который будет идти взаимодействие
+    # execute - выполняет SQL запрос
+    cur.execute(''' CREATE TABLE IF NOT EXISTS users( 
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                FName TEXT NOT NULL,
+                Doljnost TEXT NOT NULL,
+                ORab INTEGER NOT NULL,
+                ZP INTEGER
+                )
+                ''')
