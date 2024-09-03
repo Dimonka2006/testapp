264c4c680fbe305cbe06338f9080b6ccde1a61fd
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-09-03 21:18:18 +0500

    2 var

--- new_mes.sh
+++ new_mes.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+cd /home/dus
 
-pfile=$(find -name "messages * " -type f)
+pfile=$(find -name "message*" -type f)
 
 search_str=$(cat $pfile | grep 'SUCCESS LOGIN')
 
-$search_str | cut -d " " -f7 && echo " " && 
+{$search_str | cut -d " " -f7 && echo " " && 
 $search_str | cut -d " " -f8 && echo " " && 
-$search_str | cut -d " " -f11 > logins_date
+$search_str | cut -d " " -f11} > logins_date
---
+++ new_mes_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+cd /home/dus
+
+(cat messages | grep 'SUCCESS LOGIN' |cut -d " " -f7 && 
+cat messages | grep 'SUCCESS LOGIN' |cut -d " " -f8 
+cat messages | grep 'SUCCESS LOGIN' | cut -d " " -f11) > logins_date
