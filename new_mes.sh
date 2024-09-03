1f785142472d2b839e105006056dfc79a379cf81
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-09-03 22:41:48 +0500

    pravka 3

--- new_mes.sh
+++ new_mes.sh
@@ -2,10 +2,15 @@
 set -e
 cd /home/dus
 
-pfile=$(find -name "message*" -type f)
+touch logins_{date +%Y%m%d}
+
+pfile=$(find . -maxdepth 1 -name "message*" -printf " %f ")
 
 search_str=$(cat $pfile | grep 'SUCCESS LOGIN')
 
-{$search_str | cut -d " " -f7 && echo " " && 
-$search_str | cut -d " " -f8 && echo " " && 
-$search_str | cut -d " " -f11} > logins_date
+time_log=$($search_str | cut -d " " -f7)
+date_log=$($search_str | cut -d " " -f8)
+user_log=$($search_str | cut -d " " -f14)
+
+echo "$time_log $date_log $user_log" >> logins_
+#&& echo " " && 
--- new_mes_1.sh
+++ new_mes_1.sh
@@ -2,6 +2,6 @@
 set -e
 cd /home/dus
 
-(cat messages | grep 'SUCCESS LOGIN' |cut -d " " -f7 && 
+cat messages | grep 'SUCCESS LOGIN' |cut -d " " -f7 && 
 cat messages | grep 'SUCCESS LOGIN' |cut -d " " -f8 
-cat messages | grep 'SUCCESS LOGIN' | cut -d " " -f11) > logins_date
+cat messages | grep 'SUCCESS LOGIN' | cut -d " " -f11 > logins_date
