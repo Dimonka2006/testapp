a5a30a3dda6e04ed39a1368b428b4a6d25b6c765
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-09-04 14:02:54 +0500

    ver 4

--- new_mes.sh
+++ new_mes.sh
@@ -12,5 +12,8 @@ time_log=$($search_str | cut -d " " -f7)
 date_log=$($search_str | cut -d " " -f8)
 user_log=$($search_str | cut -d " " -f14)
 
-echo "$time_log $date_log $user_log" >> logins_
+echo "$time_log $date_log $user_log" >> logins_{date +%Y%m%d}
 #&& echo " " && 
+done
+new_mes.sh (END)
+
