2a376400755c207a39bf4d7b89925eb927089aa5
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-09-04 17:42:38 +0500

    ver 5

--- new_mes.sh
+++ new_mes.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-cd /home/dus
 
 touch logins_{date +%Y%m%d}
 
-pfile=$(find . -maxdepth 1 -name "message*" -printf " %f ")
+pfile=$(find . -maxdepth 1 -name "message*" -printf " %f\n ")
 
 search_str=$(cat $pfile | grep 'SUCCESS LOGIN')
 
@@ -13,7 +12,6 @@ date_log=$($search_str | cut -d " " -f8)
 user_log=$($search_str | cut -d " " -f14)
 
 echo "$time_log $date_log $user_log" >> logins_{date +%Y%m%d}
-#&& echo " " && 
+
 done
-new_mes.sh (END)
 
