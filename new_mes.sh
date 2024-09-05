184409d8a8e757c29e0f567efdca9bbc54333017
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-09-05 13:46:52 +0500

    Ver 6

--- new_mes.sh
+++ new_mes.sh
@@ -9,9 +9,12 @@ search_str=$(cat $pfile | grep 'SUCCESS LOGIN')
 
 time_log=$($search_str | cut -d " " -f7)
 date_log=$($search_str | cut -d " " -f8)
-user_log=$($search_str | cut -d " " -f14)
+user_log=$($search_str | cut -d " " -f15)
+
+# time_log=$(cat messages | grep 'SUCCESS LOGIN' | cut -d " " -f7)
+# date_log=$(cat messages | grep 'SUCCESS LOGIN' | cut -d " " -f8)
+# user_log=$(cat messages | grep 'SUCCESS LOGIN' | cut -d " " -f15)
 
 echo "$time_log $date_log $user_log" >> logins_{date +%Y%m%d}
 
-done
 
--- new_mes_1.sh
+++ new_mes_1.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
-cd /home/dus
+touch logins_$(date +%Y%m%d)
 
-cat messages | grep 'SUCCESS LOGIN' |cut -d " " -f7 && 
-cat messages | grep 'SUCCESS LOGIN' |cut -d " " -f8 
-cat messages | grep 'SUCCESS LOGIN' | cut -d " " -f11 > logins_date
+pfile=$(find . -maxdepth 1 -name "message*" -printf "%f\n")
+
+search_str=$(cat $pfile | grep 'SERVER: INFO - SUCCESS LOGIN by ')
+
+if [[ -n $search_str ]]; then
+    time_log=$(echo "$search_str" | cut -d " " -f7)
+    date_log=$(echo "$search_str" | cut -d " " -f8)
+    user_name_log=$(echo "$search_str" | cut -d " " -f14)
+    user_famil_log=$(echo "$search_str" | cut -d " " -f15)
+   # echo -e "$time_log $date_log $user_log $user_famil_log" >> logins_$(date +%Y%m%d)
+    echo -e "${time_log} ${date_log} ${user_log} ${user_famil_log}" > logins_$(date +%Y%m%d)
+   # echo "${time_log} ${date_log} ${user_log} ${user_famil_log}" >> logins_$(date +%Y%m%d)
+fi
