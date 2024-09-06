99c6919a42f9a331151fa480de3a4c29510001fe
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-09-06 15:51:41 +0500

    ver 7

--- new_mes.sh
+++ new_mes.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -e
-touch logins_$(date +%Y%m%d)
+
 
 pfile=$(find . -maxdepth 1 -name "message*" -printf "%f\n")
 
 search_str=$(cat $pfile | grep 'SERVER: INFO - SUCCESS LOGIN by ')
 
 if [[ -n $search_str ]]; then
-    # time_log=$(echo "$search_str" | cut -d " " -f7)
-    # date_log=$(echo "$search_str" | cut -d " " -f8)
-    # user_name_log=$(echo "$search_str" | cut -d " " -f14)
-    # user_famil_log=$(echo "$search_str" | cut -d " " -f15)
 
-    echo "$search_str" | cut -d " " -f3,14,15 >> logins_$(date +%Y%m%d)
+    date_log=$(echo "$search_str" | cut -d " " -f5)
+    data=$(echo "$seach_str") 
+    for i in $data
+    do 
+    file_data=$($i | cut -d " " -f5)
+    echo $i | cut -d " " -f3,14,15 > logins_$file_data
+    #touch logins_$date_log
+    echo "$search_str" | cut -d " " -f3,14,15 >> logins_$date_log
+    done
 fi
