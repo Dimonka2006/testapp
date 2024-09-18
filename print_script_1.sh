c3a6e8beebf80f87463143fdbaf440c7be7d08aa
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-09-18 17:59:13 +0500

    ver_2

---
+++ print_script_1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# подсчитываю кол-во входных переменных
+# если не равно 2 пишет ошибку и закрывает программу
+if [[ $# != 2 ]] ; then
+    echo "Usage: $0 --host HOSTNAME --timeout SECONDS" >&2
+    exit 1
+fi
+
+if [[ "$1" == "--host" && "$2" == "--timeout" ]] ; then
+   
+    host="$1"
+    timeout="$2"
+else
+    echo "Введен неверный формат параметров." >&2
+    exit 1
+fi
+
+ping -c 4 -w "${timeout}" "${host}"
+
+#./ping_script.sh --host HOSTNAME --timeout SECONDS ./ping_script_1.sh --host example.com --timeout 5
+
+# HOSTNAME – это имя хоста, а SECONDS – количество секунд.
